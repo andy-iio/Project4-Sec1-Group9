e139b8bd390ac848081f2ead166dde993a765e95
LexBwmn <Abowman8556@icloud.com>
2025-03-15 10:51:30 -0400

    Basic Structure for my CNN

--- Image_Classifier/Image_Classifier.py
+++ Image_Classifier/Image_Classifier.py
@@ -6,9 +6,25 @@ import tensorflow as tf
 import numpy as np
 
 # prepare our data
+# load data
+    # label used to read data
+# size of images (224 pixels)
+image_size = 224
+    #get data and resize images
+#function that we be used to load the data
+def load_data():
+        # some code   
+
+#Vizulization of my data (this is just for my understanding)
+
+#Data Preprocessing
+    #Noramlize Data
+    #Data Augmentation 
 
 # trian / test spilt
 
 # trian classifier 
 
-# tes performance 
+# test performance 
+    #do this using plt and graphs to validate accuracy 
+        #ex. 84% accuracy in catrogry [x]

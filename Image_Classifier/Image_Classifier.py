7a23e82bf9614a9bf60f1a81100d6bbf22d48c06
LexBwmn <Abowman8556@icloud.com>
2025-03-15 09:58:58 -0400

    Libraries for Model

---
+++ Image_Classifier/Image_Classifier.py
@@ -0,0 +1,14 @@
+# some of the required libraries for classification 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+import tensorflow as tf
+import numpy as np
+
+# prepare our data
+
+# trian / test spilt
+
+# trian classifier 
+
+# tes performance 
